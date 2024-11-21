cc85c40a18e0f855073ae7ef71ee5c52ff3b9529
abraao2005 <abraao20001@gmail.com>
2024-11-21 12:30:56 -0300

    fix: adicionando tradução dos inputs do configureproductdomain

--- lang/english.php
+++ lang/english.php
@@ -3,8 +3,11 @@
 $_LANG['isWhatsapp'] = 'Is this number WhatsApp?';
 $_LANG['neighborhood'] = 'Neighborhood';
 $_LANG["number"] = "Number";
-$_LANG['requiredFieldsNotFilled'] = "Form incomplete";
+$_LANG['requiredFieldsNotFilled'] = "Incomplete form";
 $_LANG['requiredFieldsMessage'] = "Please fill in all required fields.";
 $_LANG['requiredFieldMessage'] = "Please fill in the field ";
 $_LANG['passwordsDoNotMatch'] = "Passwords do not match";
 $_LANG['fullNameError'] = "The full name must contain at least two words";
+$_LANG["lkn_register_domain"] = "Register a new domain quickly.";
+$_LANG["lkn_transfer_domain"] = "Transfer domain to Link Nacional (used when you already have a domain registered with another provider and want to transfer it to Link Nacional)";
+$_LANG["lkn_my_domain"] = "I already have a domain registered with another provider.";
--- lang/portuguese-br.php
+++ lang/portuguese-br.php
@@ -9,3 +9,6 @@ $_LANG['requiredFieldsMessage'] = "Por favor preencha todos os campos obrigatór
 $_LANG['requiredFieldMessage'] = "Por favor preencha o campo ";
 $_LANG['passwordsDoNotMatch'] = "As senhas não conferem";
 $_LANG['fullNameError'] = "O nome completo deve conter pelo menos duas palavras";
+$_LANG["lkn_register_domain"]= "Registrar um novo domínio rapidamente.";
+$_LANG["lkn_transfer_domain"]= "Transferir domínio para Link Nacional (utilizado quando você já tem um domínio registrado em outro provedor e deseja transferir para a Link Nacional)";
+$_LANG["lkn_my_domain"]= "Já tenho um domínio registrado em outro provedor.";
